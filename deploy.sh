bda83a729991ec40c9694b596c7038e8673dd0b1
Ubuntu <ubuntu@ip-172-31-47-116.ap-southeast-1.compute.internal>
2024-05-20 02:34:34 +0000

    update files

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+DEV_REPO="vaishnd/dev"
+PROD_REPO="vaishnd/prod"
+IMAGE_NAME="capstone"
+DOCKER_USERNAME="vaishnd"
+DOCKER_PASSWORD="May@2024."
+
+
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 if [[ $GIT_BRANCH == "origin/main" ]]; then
-    docker tag capstone vaishnd/prod
-    docker push vaishnd/prod
+    docker tag capstone vaishnd/prod:latest
+    docker push vaishnd/prod:latest
 elif [[ $GIT_BRANCH == "origin/dev" ]]; then
-    docker tag capstone vaishnd/dev
-    docker push vaishnd/dev
+    docker tag capstone vaishnd/dev:latest
+    docker push vaishnd/dev:latest
 fi
